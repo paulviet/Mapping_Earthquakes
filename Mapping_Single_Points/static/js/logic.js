43f10984cc73822c0f2161d2a45a05a7e7ea9739
Paul Do <9637489+paulviet@users.noreply.github.com>
2021-05-12 07:28:27 -0400

    13.4.1 Branch Mapping_Single_Points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-var map = L.map('mapid').setView([40.7, -94.5], 4);
+var map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Alternative to setView above
 // let map = L.map("mapid", {
@@ -16,10 +16,29 @@ var map = L.map('mapid').setView([40.7, -94.5], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     // https://docs.mapbox.com/api/maps/styles/
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(map); // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map)
+//     .bindPopup('Welcome to LA.<br> go see LACMA Lights.')
+//     .openPopup();
+
+// let marker = L.circle([34.0522, -118.2437], {
+//     radius: 100
+//     }).addTo(map)
+//     .bindPopup('Welcome to LA.<br> go see LACMA Lights.')
+//     .openPopup();
+
+let marker = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+    }).addTo(map)
+    .bindPopup('Welcome to LA.<br> go see LACMA Lights.')
+    .openPopup();
